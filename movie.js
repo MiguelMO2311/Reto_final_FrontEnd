84e2eded177b790a41401f4b753b30486f616f9d
MiguelMO2311 <mmeneses73@gmail.com>
2024-01-14 10:32:55 +0100

    ok proyecto final front-End Imdb

--- movie.js
+++ movie.js
@@ -1,45 +1,42 @@
+
+class Movie {
+
+  constructor(Id, title, releaseYear, nacionality, genre, photo) {
+    this.Id = Id;
+    this.title = title;
+    this.releaseYear = releaseYear;
+    this.nacionality = nacionality;
+    this.genre = genre;
+    this.photo = photo;
+    this.actors = [];
+    this.director = '';
+    this.writer = '';;
+    this.language = '';
+    this.plataforma = '';
+    this.isMCU = true;
+    this.mainCharacterName = '';
+    this.producer = '';
+    this.distributor = '';
+  }
+
+}
+const dataBase = [
+  new Movie(1, 'El Rey Leon', 1994, 'United States of America', 'musical', './img/img_elReyLeon.jpeg'),
+  new Movie(2, 'Mision Imposible- Sentencia Mortal_1', 2023, 'United States of America', 'action', './img/img_misionImposible7.jpg'),
+  new Movie(3, 'Wonka', 2023, 'United Kingdom', 'fantastic', './img/img_Wonka.jpg'),
+  new Movie(4, 'Los Juegos del Hambre- Balada…', 2023, 'United States of America', 'science-fiction', './img/img_losJuegosDelHambre.jpg'),
+  new Movie(5, 'Aquaman y el Reino Perdido', 2023, 'United States of America', 'fantastic', './img/img_Acuaman.jpg'),
+  new Movie(6, 'Champions', 2023, 'United States of America', 'comedy', './img/img_Champions.jpg'),
+];
+
 const FilmsContainer = [];
 const container = document.getElementById('films-container');
 
-class Movie {
-   
-    constructor (Id, title, releaseYear, nacionality, genre, photo){
-        this.Id = Id;
-        this.title = title;
-        this.releaseYear =releaseYear;
-        this.nacionality = nacionality;
-        this. genre = genre;
-        this.photo = photo;  
-
-        this.actors = [];
-        this.director = '';
-        this.writer = '';        ;
-        this.language = '';
-        this.plataforma = '';
-        this.isMCU = true;
-        this.mainCharacterName = '';
-        this.producer = '';
-        this.distributor = '';
-        
-}
-}
-const dataBase = [
-    new Movie (1,'El Rey Leon',1994,'United States of America', 'musical','./img/img_elReyLeon.jpeg' ),
-    new Movie (2, 'Mision Imposible- Sentencia Mortal_1', 2023, 'United States of America','action','./img/img_misionImposible7.jpg'),
-    new Movie (3,'Wonka',2023, 'United Kingdom', 'fantastic','./img/img_Wonka.jpg' ),
-    new Movie (4,'Los Juegos del Hambre- Balada…', 2023, 'United States of America','science-fiction','./img/img_losJuegosDelHambre.jpg'),
-    new Movie (5,'Aquaman y el Reino Perdido',2023,'United States of America', 'fantastic','./img/img_Acuaman.jpg' ),
-    new Movie (6,'Champions', 2023, 'United States of America','comedy','./img/img_Champions.jpg'),
-    ];
-    
-   
- 
-   
-    function createCards() {
-        let htmlCards = '';
-        dataBase.forEach(movie => {
-            const card =
-    `<div class="card" style="width: 14rem; margin:2%; padding:2%; border:none;">
+function createCards() {
+  let htmlCards = '';
+  dataBase.forEach(movie => {
+    const card =
+      `<div class="card" style="width: 14rem; margin:2%; padding:2%; border:none;">
       <img src="${movie.photo}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${movie.title}</h5>
@@ -49,48 +46,50 @@ const dataBase = [
       
       </div>
     </div>`
-            htmlCards += card;
-        })
-        container.innerHTML = htmlCards;
+    htmlCards += card;   
+  })
+  container.innerHTML = htmlCards;
+}
+
+// para añadir y mostrar nuevas peliculas ***
+
+const newItemContainer = document.getElementById('newItemContainer');
+function addNewItem() {
+    const IdInput = document.getElementById('Id').value;
+    const photoInput = document.getElementById('photo').value;
+    const titleInput = document.getElementById('title').value;
+    const releaseYearInput = document.getElementById('releaseYear').value;
+    const nacionalityInput = document.getElementById('nacionality').value;
+    const genreInput = document.getElementById('genre').value;
+    
+
+    const newItem = {
+        itemId: IdInput,
+        itemPhoto: photoInput,
+        itemTitle: titleInput,
+        itemReleaseYear: releaseYearInput,
+        itemNacionality: nacionalityInput,
+        itemGenre: genreInput,
+        }
+
+        const newItemCard = document.createElement('ul');
+        newItemCard.innerHTML =  `<div class="card" style="width: 14rem; margin:2%; padding:2%; border:none;">
+        <img src="${newItem.itemPhoto}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${newItem.itemTitle}</h5>
+          <p class="card-text">${newItem.itemNacionality}</p>
+          <p class="card-text">${newItem.itemReleaseYear}</p>
+          <p class="card-text">${newItem.itemGenre}</p>
+        
+        </div>
+      </div>`;
+        newItemContainer.appendChild(newItemCard);
+        container.innerText = newItemCard;
     }
 
+// para almacenar en el LocalStorage ***
+localStorage.setItem('dataBase', JSON.stringify ('dataBase'));
 
-
- 
-
-
-
-
-
-//     Movie.prototype.showAllMovieData = function () {
-//         var _this = this;
-//         actorsName = this.actors.map(function (actor) { return _this.actors; });
-//         var mv = "Title: ".concat(this.title, ",\n     releaseYear: ").concat(this.releaseYear, ",\n   actors: ").concat(this.actors, ",\n     nacionality: ").concat(this.nacionality, ",\n     director: ").concat(this.director, ",\n     writer: ").concat(this.writer, ",\n     language: ").concat(this.language, ",\n     plataforma: ").concat(this.plataforma, ",\n     isMCU: boolean;").concat(this.isMCU, ",\n     mainCharacterName: ").concat(this.mainCharacterName, ",\n     producer: ").concat(this.producer, ",\n     distributor: ").concat(this.distributor, ",\n     genre: ").concat(this.genre, ".");
-//         return mv;
-//     };
-//     return Movie;
-// };
-
-
-// showAllMovieData(){
-//     let actorsName = this.actors.map((actor) => this.actors)
-
-//     let mv = 
-//     `Title: ${this.title},
-//      releaseYear: ${this.releaseYear},
-//      actors: ${this.actors},
-//      nacionality: ${this.nacionality},
-//      director: ${this.director},
-//      writer: ${this.writer},
-//      language: ${this.language},
-//      plataforma: ${this.plataforma},
-//      isMCU: boolean;${this.isMCU},
-//      mainCharacterName: ${this.mainCharacterName},
-//      producer: ${this.producer},
-//      distributor: ${this.distributor},
-//      genre: ${this.genre}.`
-    
-    
-//     return mv;
-// }
+// para recuperar del LocalStorage ***
+dataBase =JSON.parse(localStorage.getItem('dataBase'));
 
--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-
-
-function addNewItem() {
-    const IdInput = document.getElementById('Id').value;
-    const photoInput = document.getElementById('photo').value;
-    const titleInput = document.getElementById('title').value;
-    const releaseYearInput = document.getElementById('releaseYear').value;
-    const nacionalityInput = document.getElementById('nacionality').value;
-    const genreInput = document.getElementById('genre').value;
+// const newItemContainer = document.getElementById('newItemContainer');
+// function addNewItem() {
+//     const IdInput = document.getElementById('Id').value;
+//     const photoInput = document.getElementById('photo').value;
+//     const titleInput = document.getElementById('title').value;
+//     const releaseYearInput = document.getElementById('releaseYear').value;
+//     const nacionalityInput = document.getElementById('nacionality').value;
+//     const genreInput = document.getElementById('genre').value;
     
 
-    const newItem = {
-        itemId: IdInput,
-        itemPhoto: photoInput,
-        itemTitle: titleInput,
-        itemReleaseYear: releaseYearInput,
-        itemNacionality: nacionalityInput,
-        itemGenre: genreInput,
+//     const newItem = {
+//         itemId: IdInput,
+//         itemPhoto: photoInput,
+//         itemTitle: titleInput,
+//         itemReleaseYear: releaseYearInput,
+//         itemNacionality: nacionalityInput,
+//         itemGenre: genreInput,
+//         }
+
+//         const newItemCard = document.createElement('ul');
+//         newItemCard.innerHTML =  `<div class="card" style="width: 14rem; margin:2%; padding:2%; border:none;">
+//         <img src="${newItem.itemPhoto}" class="card-img-top" alt="...">
+//         <div class="card-body">
+//           <h5 class="card-title">${newItem.itemTitle}</h5>
+//           <p class="card-text">${newItem.itemNacionality}</p>
+//           <p class="card-text">${newItem.itemReleaseYear}</p>
+//           <p class="card-text">${newItem.itemGenre}</p>
         
+//         </div>
+//       </div>`;
+//         newItemContainer.appendChild(newItemCard);
+//         container.innerText = newItemCard;
+//     }
 
-    }
-    dataBase.push (newItem);
-    console.log(dataBase);
-
-
-};
-
-
-localStorage.setItem('dataBase', JSON.stringify ('dataBase'));
-JSON.parse(localStorage.getItem('dataBase'));
+// localStorage.setItem('dataBase', JSON.stringify ('dataBaseUser'));
+// JSON.parse(localStorage.getItem('dataBase'));
